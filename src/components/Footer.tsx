b1ede0176e9c55bdaf51375ad45cb8cef7d33fab
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-27 18:44:33 +0530

    Add files via upload

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,11 +27,11 @@ export const Footer = () => {
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            / Powered by vision. Driven by design.{" "}
             <SmartLink
-              href="https://once-ui.com/templates/magic-portfolio"
+              href="https://artistrydesigndiaries.blogspot.com/"
             >
-              Once UI
+              Artistry
             </SmartLink>
           </Text>
         </Text>
