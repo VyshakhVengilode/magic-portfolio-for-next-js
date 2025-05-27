5056ec192864260558c9e4edc325f49413d8edf1
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-27 17:35:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
               {home.subline}
             </Text>
           </RevealFx>
-          <RevealFx paddingTop="12" delay={0.4} horizontal="start" paddingLeft="12">
+          <RevealFx paddingTop="12" delay={0.4} horizontal="center">
             <Button
               id="about"
               data-border="rounded"
