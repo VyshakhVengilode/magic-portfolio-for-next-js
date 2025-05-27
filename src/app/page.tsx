f2bc10d09d0cd589bed4aa0d3bd3e5d4c9df74cd
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-27 17:52:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,49 +37,58 @@ export default function Home() {
           image: `${baseURL}${person.avatar}`,
         }}
       />
-      <Column fillWidth paddingY="24" gap="m">
-        <Column maxWidth="s">
-          {home.featured && (
-          <RevealFx fillWidth horizontal="start" paddingTop="16" paddingBottom="32" paddingLeft="12">
-            <Badge background="brand-alpha-weak" paddingX="12" paddingY="4" onBackground="neutral-strong" textVariant="label-default-s" arrow={false}
-              href={home.featured.href}>
-              <Row paddingY="2">{home.featured.title}</Row>
-            </Badge>
-          </RevealFx>
-          )}
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="16">
-            <Heading wrap="balance" variant="display-strong-l">
-              {home.headline}
-            </Heading>
-          </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="32">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
-              {home.subline}
-            </Text>
-          </RevealFx>
-          <RevealFx paddingTop="12" delay={0.4} horizontal="start" paddingLeft="12">
-            <Button
-              id="about"
-              data-border="rounded"
-              href={about.path}
-              variant="secondary"
+<Column fillWidth paddingY="24" gap="m">
+  <Column maxWidth="s">
+    {home.featured && (
+      <RevealFx fillWidth horizontal="center" paddingTop="16" paddingBottom="32">
+        <Badge background="brand-alpha-weak" paddingX="12" paddingY="4" onBackground="neutral-strong" textVariant="label-default-s" arrow={false}
+          href={home.featured.href}>
+          <Row paddingY="2">{home.featured.title}</Row>
+        </Badge>
+      </RevealFx>
+    )}
+    <RevealFx translateY="4" fillWidth horizontal="center" paddingBottom="16">
+      <Heading
+        wrap="balance"
+        variant="display-strong-l"
+        style={{ textAlign: "center" }}
+      >
+        {home.headline}
+      </Heading>
+    </RevealFx>
+    <RevealFx translateY="8" delay={0.2} fillWidth horizontal="center" paddingBottom="32">
+      <Text
+        wrap="balance"
+        onBackground="neutral-weak"
+        variant="heading-default-xl"
+        style={{ textAlign: "center" }}
+      >
+        {home.subline}
+      </Text>
+    </RevealFx>
+    <RevealFx paddingTop="12" delay={0.4} horizontal="center">
+      <Button
+        id="about"
+        data-border="rounded"
+        href={about.path}
+        variant="secondary"
+        size="m"
+        arrowIcon
+      >
+        <Flex gap="8" vertical="center">
+          {about.avatar.display && (
+            <Avatar
+              style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
+              src={person.avatar}
               size="m"
-              arrowIcon
-            >
-              <Flex gap="8" vertical="center">
-                {about.avatar.display && (
-                  <Avatar
-                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
-                )}
-                {about.title}
-              </Flex>
-            </Button>
-          </RevealFx>
-        </Column>
-      </Column>
+            />
+          )}
+          {about.title}
+        </Flex>
+      </Button>
+    </RevealFx>
+  </Column>
+</Column>
       {routes["/blog"] && (
         <Flex fillWidth gap="24" mobileDirection="column">
           <Flex flex={1} paddingLeft="l" paddingTop="24">
