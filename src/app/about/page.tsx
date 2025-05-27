b65445e817970baca5a02e87736fe0345b600e9c
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-27 13:48:01 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -299,13 +299,13 @@ export default function About() {
                 as="h2"
                 id={about.publicationsSection.title}
                 variant="display-strong-s"
-                marginBottom="12"
+                marginBottom="8"
               >
                 {about.publicationsSection.title}
               </Heading>
               <Text
                 variant="heading-default-s"
-                marginBottom="12"
+                marginBottom="18"
                 onBackground="neutral-weak"
               >
                 {about.publicationsSection.subtitle}
