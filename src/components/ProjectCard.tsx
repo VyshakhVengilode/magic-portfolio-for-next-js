0a3ebbfd189f1e4bb0248e03e9350013d0cd30e1
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-28 14:52:58 +0530

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -34,6 +34,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
     <Column fillWidth gap="m">
       <Carousel
         sizes="(max-width: 960px) 100vw, 960px"
+        style={{ maxHeight: "679px", width: "100%", height: "auto" }}
         images={images.map((image) => ({
           src: image,
           alt: title,
