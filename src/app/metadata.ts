9ec773603ce76a0897c9635bc228b08f1953b8b4
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-29 14:09:57 +0530

    Create metadata.ts

---
+++ src/app/metadata.ts
@@ -0,0 +1,25 @@
+export const metadata = {
+  title: "Vyshakh Vengilode's Portfolio",
+  description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+  openGraph: {
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+        width: 1200,
+        height: 630,
+        alt: "Vyshakh Vengilode's Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+    ],
+  },
+};
