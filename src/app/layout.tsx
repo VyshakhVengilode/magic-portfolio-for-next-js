a5c225c447008deb53acf3fae424fffa598d773a
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-29 12:02:31 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,31 @@ import { Background, Column, Flex, ThemeProvider, ToastProvider } from "@/once-u
 import { opacity, SpacingToken } from "@/once-ui/types";
 import { Meta } from "@/once-ui/modules";
 
-export async function generateMetadata() {
-  return Meta.generate({
-    image: home.image,
-    title: home.title,
-    description: home.description,
-    baseURL: baseURL,
-    path: home.path,    
-  });
-}
+export const metadata = {
+  title: "Vyshakh Vengilode's Portfolio",
+  description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+  openGraph: {
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+        width: 1200,
+        height: 630,
+        alt: "Vyshakh Vengilode's Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+    ],
+  },
+};
 
 interface RootLayoutProps {
   children: React.ReactNode;
