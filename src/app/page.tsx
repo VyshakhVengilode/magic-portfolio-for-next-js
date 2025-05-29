80d30e1e75a5500982ed691a5c8ea86ccf09591b
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-05-29 07:45:42 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,31 @@ import { Posts } from "@/components/blog/Posts";
 import { Meta, Schema } from "@/once-ui/modules";
 import styles from "@/components/about/about.module.scss";
 
-export async function generateMetadata() {
-  return Meta.generate({
-    title: home.title,
-    description: home.description,
-    baseURL: baseURL,
-    path: home.path,
-  });
-}
+export const metadata = {
+  title: "Vyshakh Vengilode's Portfolio",
+  description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+  openGraph: {
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+        width: 1200,
+        height: 630,
+        alt: "Vyshakh Vengilode's Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vyshakh Vengilode's Portfolio",
+    description: "Portfolio website showcasing my work as a Graphic Designer | Writer | Developer",
+    images: [
+      "https://raw.githubusercontent.com/VyshakhVengilode/magic-portfolio-for-next-js/refs/heads/main/public/images/projects/project-01/portfolio-01.jpg?token=GHSAT0AAAAAADEPI7CMWYWKROHQJFJC3YGG2BXYKWQ",
+    ],
+  },
+};
 
 export default function Home() {
   return (
