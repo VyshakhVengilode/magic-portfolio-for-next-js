4eae63950e31703b9fe2e8e329418bf328c06107
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-06-14 23:05:25 +0530

    Update Page.tsx

--- src/app/fav/Page.tsx
+++ src/app/fav/Page.tsx
@@ -1,25 +1,5 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import "../components/PersonalFavorites.module.css";
+import PersonalFavorites from "@/components/fav/PersonalFavorites";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        {/* Malayalam font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@700&display=swap"
-          rel="stylesheet"
-        />
-        {/* Roboto font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function Page() {
+  return <PersonalFavorites />;
 }
-
-export default MyApp;
