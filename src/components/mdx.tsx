4d639a8136a3608ee220b4c9c8de9e7cdabc7a7c
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-10-24 14:17:39 +0530

    Update mdx.tsx

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -55,7 +55,7 @@ function createImage({ alt, src, ...props }: SmartImageProps & { src: string }) 
       marginBottom="16"
       enlarge
       radius="m"
-      aspectRatio="auto"
+      aspectRatio=""
       border="neutral-alpha-medium"
       sizes="(max-width: 960px) 100vw, 960px"
       alt={alt}
@@ -181,3 +181,4 @@ export function CustomMDX(props: CustomMDXProps) {
   );
 
 }
+
