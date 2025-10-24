62c958d9c57be81d4e9da7e7d65d566febd431ba
VyshakhVengilode <47266340+VyshakhVengilode@users.noreply.github.com>
2025-10-24 14:15:11 +0530

    Update mdx.tsx

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -55,7 +55,7 @@ function createImage({ alt, src, ...props }: SmartImageProps & { src: string }) 
       marginBottom="16"
       enlarge
       radius="m"
-      aspectRatio="16 / 9"
+      aspectRatio="auto"
       border="neutral-alpha-medium"
       sizes="(max-width: 960px) 100vw, 960px"
       alt={alt}
@@ -179,4 +179,5 @@ export function CustomMDX(props: CustomMDXProps) {
   return (
     <MDXRemote {...props} components={{ ...components, ...(props.components || {}) }} />
   );
+
 }
